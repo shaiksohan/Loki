dfec784a3314f797a3644a8e8da68876b14f038c
shaiksohan <shaiksohan187@gmail.com>
2026-01-05 23:14:09 +0530

    delete perfect

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 let input = document.getElementById("taskInput");
 let list = document.getElementById("taskList");
 
-/* Load saved tasks */
-
+/* Load saved tasks on refresh */
 window.onload = function () {
-    let savedTasks = localStorage.getItem("tasks");
-    if (savedTasks) {
-        list.innerHTML = savedTasks;
+    let tasks = localStorage.getItem("tasks");
+    if (tasks) {
+        list.innerHTML = tasks;
     }
 };
-/* Add task */
+
+/* ADD TASK (SIMPLIFIED) */
 function addTask() {
     if (input.value === "") {
         alert("Please enter a task");
         return;
-        
     }
 
     let li = document.createElement("li");
-
     li.innerHTML =
         "<span>" + input.value + "</span>" +
-        "<button onclick='deleteTask(event, this)'>Delete</button>";
+        "<button onclick='deleteTask(this)'>Delete</button>";
 
+    /* Mark completed */
     li.querySelector("span").onclick = function () {
         li.classList.toggle("completed");
         saveTasks();
@@ -33,29 +32,13 @@ function addTask() {
     saveTasks();
 }
 
-/* Delete single task */
-function deleteTask(event, button) {
-    event.stopPropagation();
+/* DELETE SINGLE TASK */
+function deleteTask(button) {
     button.parentElement.remove();
     saveTasks();
 }
 
-/* ✅ DELETE ALL TASKS (FIXED) */
-function deleteAllTasks() {
-    if (list.children.length === 0) {
-        alert("No tasks to delete");
-        return;
-    }
-
-    if (confirm("Are you sure you want to delete all tasks?")) {
-        list.innerHTML = "";
-        localStorage.removeItem("tasks");
-    }
-}
-
-/* Save tasks */
+/* SAVE TASKS */
 function saveTasks() {
     localStorage.setItem("tasks", list.innerHTML);
 }
-
-
