4a0b56f83f24a2e396b774a9f6549ee95d6c03b6
shaiksohan <shaiksohan187@gmail.com>
2026-01-05 23:44:33 +0530

    Upadated to without queries

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let input = document.getElementById("taskInput");
 let list = document.getElementById("taskList");
 
-/* Load saved tasks on refresh */
+/* Load tasks on refresh */
 window.onload = function () {
     let tasks = localStorage.getItem("tasks");
     if (tasks) {
@@ -9,7 +9,7 @@ window.onload = function () {
     }
 };
 
-/* ADD TASK (SIMPLIFIED) */
+/* Add task */
 function addTask() {
     if (input.value === "") {
         alert("Please enter a task");
@@ -17,28 +17,29 @@ function addTask() {
     }
 
     let li = document.createElement("li");
-    li.innerHTML =
-        "<span>" + input.value + "</span>" +
-        "<button onclick='deleteTask(this)'>Delete</button>";
+    let span = document.createElement("span");
+    let btn = document.createElement("button");
 
-    /* Mark completed */
-    li.querySelector("span").onclick = function () {
-        li.classList.toggle("completed");
-        saveTasks();
-    };
+    span.innerText = input.value;
+    btn.innerText = "Delete";
 
+    btn.setAttribute("onclick", "deleteTask(this)");
+
+    li.appendChild(span);
+    li.appendChild(btn);
     list.appendChild(li);
+
     input.value = "";
     saveTasks();
 }
 
-/* DELETE SINGLE TASK */
-function deleteTask(button) {
-    button.parentElement.remove();
+/* Delete task */
+function deleteTask(btn) {
+    btn.parentElement.remove();
     saveTasks();
 }
 
-/* SAVE TASKS */
+/* Save tasks */
 function saveTasks() {
     localStorage.setItem("tasks", list.innerHTML);
 }
